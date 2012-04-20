d09df5e0847f17ecc4b221177f19e42ccd80beba
Marc Love <marcslove@gmail.com>
2012-04-20 12:17:15 -0700

    Add configuration block.

---
+++ README.markdown
@@ -0,0 +1,2 @@
+# Soothsayer
+Soothsayer is an easy Ruby client for Google's Prediction API. It is still very alpha and a proper README will be forthcoming.
--- lib/soothsayer.rb
+++ lib/soothsayer.rb
@@ -6,6 +6,30 @@ require 'google/api_client'
 require 'multi_json'
 
 module Soothsayer
+  def self.credentials
+    @@config
+  end
+
+  # Configures Soothsayer with the credentials downloaded by the Ruby api client into .google-api.yaml
+  #
+  #   config_file = File.expand_path('~/.google-api.yaml')
+  #   config = open(config_file, 'r'){ |file| YAML.load(file.read) }
+  #   Soothsayer.config do |c|
+  #     c.client_id = config.client_id
+  #     c.client_secret = config.client_secret
+  #     c.access_token = config.access_token
+  #     c.refresh_token = config.refresh_token
+  #   end
+  #
+  def self.config(config = Configuration.new)
+    yield config
+    @@config = config
+  end
+
+  class Configuration
+    attr_accessor :client_id, :client_secret, :access_token, :refresh_token
+  end
+
   class OAuthTokenError < StandardError; end
   class API
     include HTTParty
@@ -26,15 +50,14 @@ module Soothsayer
       end
 
       def create_client
-        config_file = File.expand_path('~/.google-api.yaml')
-        config = open(config_file, 'r'){ |file| YAML.load(file.read) }
+        raise OAuthTokenError, 'Please configure Soothsayer first with Soothsayer.config.' if Soothsayer.credentials.nil?
         client = Google::APIClient.new(:authorization => :oauth_2)
         unless client.authorization.nil?
           client.authorization.scope = nil
-          client.authorization.client_id = config["client_id"]
-          client.authorization.client_secret = config["client_secret"]
-          client.authorization.access_token = config["access_token"]
-          client.authorization.refresh_token = config["refresh_token"]
+          client.authorization.client_id = Soothsayer.credentials.client_id
+          client.authorization.client_secret = Soothsayer.credentials.client_secret
+          client.authorization.access_token = Soothsayer.credentials.access_token
+          client.authorization.refresh_token = Soothsayer.credentials.refresh_token
           client.register_discovery_uri('discovery', 'v1.5', nil)
           self.api_client = client
         else
